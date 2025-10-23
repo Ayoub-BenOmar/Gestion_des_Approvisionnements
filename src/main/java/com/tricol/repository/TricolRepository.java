84cd70d3f3decf5efe77580ee62ab6188ebc0d79
ayuub <ayoubbenomar1230@gmail.com>
2025-10-23 09:06:42 +0100

    created repository interface

---
+++ src/main/java/com/tricol/repository/TricolRepository.java
@@ -0,0 +1,8 @@
+package com.tricol.repository;
+
+import com.tricol.model.Tricol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TricolRepository extends JpaRepository<Tricol, Integer> {
+
+}
