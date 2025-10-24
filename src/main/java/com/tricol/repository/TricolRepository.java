9b9cba157885c0d497790e7219ef9e8f54ecfad7
ayuub <ayoubbenomar1230@gmail.com>
2025-10-24 10:15:00 +0100

    added the tricol dao instead of the repository to fix the 500 error

--- src/main/java/com/tricol/repository/TricolRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tricol.repository;
-
-import com.tricol.model.Tricol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TricolRepository extends JpaRepository<Tricol, Integer> {
-
-}
