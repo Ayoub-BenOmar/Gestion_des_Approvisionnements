22802f55bedfb3e0153559c13364c24a81cc9e58
ayuub <ayoubbenomar1230@gmail.com>
2025-10-24 10:16:07 +0100

    updated TricolService with tricoldao

--- src/main/java/com/tricol/service/TricolService.java
+++ src/main/java/com/tricol/service/TricolService.java
@@ -1,35 +1,34 @@
 package com.tricol.service;
 
 import com.tricol.model.Tricol;
-import com.tricol.repository.TricolRepository;
+import com.tricol.repository.TricolDao;
 
 import java.util.List;
 
 public class TricolService {
-    private TricolRepository tricolRepository;
+    private TricolDao tricolDao;
 
-    public void setTricolRepository(TricolRepository tricolRepository){
-        this.tricolRepository = tricolRepository;
-    }
-
-    public Tricol save(Tricol tricol) {
-        return tricolRepository.save(tricol);
-    }
-
-    public Tricol update(Integer id, Tricol tricol) {
-        tricol.setId(id);
-        return tricolRepository.save(tricol);
-    }
-
-    public Tricol getById(Integer id) {
-        return tricolRepository.findById(id).orElse(null);
+    public void setTricolDao(TricolDao tricolDao) {
+        this.tricolDao = tricolDao;
     }
 
     public List<Tricol> getAll() {
-        return tricolRepository.findAll();
+        return tricolDao.findAll();
+    }
+
+    public Tricol getById(Integer id) {
+        return tricolDao.findById(id);
+    }
+
+    public void save(Tricol tricol) {
+        tricolDao.save(tricol);
+    }
+
+    public void update(Integer id, Tricol tricol) {
+        tricolDao.update(id, tricol);
     }
 
     public void delete(Integer id) {
-        tricolRepository.deleteById(id);
+        tricolDao.delete(id);
     }
 }
